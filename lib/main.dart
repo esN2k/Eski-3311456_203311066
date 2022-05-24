8d3ebfa338ed921a73daf5afd4828d2a59390054
Doğukan 'Esen' Balaman <dodikbalaman@gmail.com>
2022-05-24 15:28:02 +0300

    Splash screen folders and etc.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'auth/firebase_user_provider.dart';
+import 'auth/auth_util.dart';
+
+import 'esendo/esendo_theme.dart';
+import 'esendo/esendo_util.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+import 'index.dart';
 
 void main() async {
   runApp(MyApp());
 }
-
-class MyApp extends StatefulWidget {}
--- lib/splash_screen/splash_screen_widget.dart
+++ lib/splash_screen/splash_screen_widget.dart
@@ -1,1 +1,11 @@
+import '../esendo/esendo_theme.dart';
+import '../esendo/esendo_util.dart';
+import '../esendo/esendo_widgets.dart';
+import '../login/login_widget.dart';
+import '../register/register_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:google_fonts/google_fonts.dart';
+
+class SplashScreenWidget extends StatefulWidget {
+}
