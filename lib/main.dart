2bbf1b939f25c72604605bd02b7bfbab3ae20dbe
Doğukan 'Esen' Balaman <dodikbalaman@gmail.com>
2022-05-24 15:52:06 +0300

    Auths

--- lib/main.dart
+++ lib/main.dart
@@ -13,5 +13,149 @@ import 'package:google_nav_bar/google_nav_bar.dart';
 import 'index.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+
+  static _MyAppState of(BuildContext context) =>
+      context.findAncestorStateOfType<_MyAppState>();
+}
+
+class _MyAppState extends State<MyApp> {
+  Locale _locale;
+  ThemeMode _themeMode = ThemeMode.system;
+
+  Stream<EsenDoFirebaseUser> userStream;
+  EsenDoFirebaseUser initialUser;
+  bool displaySplashImage = true;
+
+  final authUserSub = authenticatedUserStream.listen((_) {});
+
+  @override
+  void initState() {
+    super.initState();
+    userStream = esenDoFirebaseUserStream()
+      ..listen((user) => initialUser ?? setState(() => initialUser = user));
+    Future.delayed(
+      Duration(seconds: 1),
+      () => setState(() => displaySplashImage = false),
+    );
+  }
+
+  @override
+  void dispose() {
+    authUserSub.cancel();
+
+    super.dispose();
+  }
+
+  void setLocale(Locale value) => setState(() => _locale = value);
+  void setThemeMode(ThemeMode mode) => setState(() {
+        _themeMode = mode;
+      });
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'EsenDo',
+      localizationsDelegates: [
+        FFLocalizationsDelegate(),
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      locale: _locale,
+      supportedLocales: const [
+        Locale('tr', ''),
+      ],
+      theme: ThemeData(brightness: Brightness.light),
+      themeMode: _themeMode,
+      home: initialUser == null || displaySplashImage
+          ? Container(
+              color: Colors.transparent,
+              child: Builder(
+                builder: (context) => Image.asset(
+                  'assets/images/Adsz_tasarm_(4).png',
+                  fit: BoxFit.cover,
+                ),
+              ),
+            )
+          : currentUser.loggedIn
+              ? NavBarPage()
+              : SplashScreenWidget(),
+    );
+  }
+}
+
+class NavBarPage extends StatefulWidget {
+  NavBarPage({Key key, this.initialPage}) : super(key: key);
+
+  final String initialPage;
+
+  @override
+  _NavBarPageState createState() => _NavBarPageState();
+}
+
+/// This is the private State class that goes with NavBarPage.
+class _NavBarPageState extends State<NavBarPage> {
+  String _currentPage = 'myTasks';
+
+  @override
+  void initState() {
+    super.initState();
+    _currentPage = widget.initialPage ?? _currentPage;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final tabs = {
+      'CompletedTasks': CompletedTasksWidget(),
+      'myTasks': MyTasksWidget(),
+      'MyProfile': MyProfileWidget(),
+    };
+    final currentIndex = tabs.keys.toList().indexOf(_currentPage);
+    return Scaffold(
+      body: tabs[_currentPage],
+      bottomNavigationBar: GNav(
+        selectedIndex: currentIndex,
+        onTabChange: (i) =>
+            setState(() => _currentPage = tabs.keys.toList()[i]),
+        backgroundColor: EsenDoTheme.of(context).primaryBlack,
+        color: EsenDoTheme.of(context).tertiaryColor,
+        activeColor: EsenDoTheme.of(context).primaryColor,
+        tabBackgroundColor: Color(0x00000000),
+        tabBorderRadius: 100,
+        tabMargin: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 0),
+        padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 0),
+        gap: 0,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        duration: Duration(milliseconds: 500),
+        haptic: false,
+        tabs: [
+          GButton(
+            icon: currentIndex == 0 ? Icons.alarm_on : Icons.alarm_on,
+            text: 'Yaptıklarım',
+            iconSize: 32,
+          ),
+          GButton(
+            icon: currentIndex == 1 ? Icons.playlist_add : Icons.playlist_add,
+            text: 'Yapılacaklarım',
+            iconSize: 32,
+          ),
+          GButton(
+            icon: currentIndex == 2 ? Icons.person_sharp : Icons.person_outline,
+            text: 'Profil',
+            iconSize: 32,
+          )
+        ],
+      ),
+    );
+  }
 }
--- lib/splash_screen/splash_screen_widget.dart
+++ lib/splash_screen/splash_screen_widget.dart
@@ -8,4 +8,152 @@ import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class SplashScreenWidget extends StatefulWidget {
+  const SplashScreenWidget({Key key}) : super(key: key);
+
+  @override
+  _SplashScreenWidgetState createState() => _SplashScreenWidgetState();
+}
+
+class _SplashScreenWidgetState extends State<SplashScreenWidget> {
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  void initState() {
+    super.initState();
+    logFirebaseEvent('screen_view',
+        parameters: {'screen_name': 'SplashScreen'});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: scaffoldKey,
+      backgroundColor: EsenDoTheme.of(context).primaryBlack,
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height * 1,
+        decoration: BoxDecoration(
+          color: EsenDoTheme.of(context).primaryBlack,
+          image: DecorationImage(
+            fit: BoxFit.fill,
+            image: Image.asset(
+              'assets/images/bg_login.png',
+            ).image,
+          ),
+        ),
+        child: Padding(
+          padding: EdgeInsetsDirectional.fromSTEB(0, 40, 0, 0),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      'assets/images/Adsz_tasarm_(4).png',
+                      width: MediaQuery.of(context).size.width * 0.5,
+                      height: MediaQuery.of(context).size.height * 0.5,
+                      fit: BoxFit.fitHeight,
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: Padding(
+                        padding: EdgeInsetsDirectional.fromSTEB(0, 20, 0, 0),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.max,
+                          children: [
+                            Padding(
+                              padding:
+                                  EdgeInsetsDirectional.fromSTEB(0, 2, 0, 20),
+                              child: FFButtonWidget(
+                                onPressed: () async {
+                                  logFirebaseEvent('Button-ON_TAP');
+                                  logFirebaseEvent('Button-Navigate-To');
+                                  await Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => RegisterWidget(),
+                                    ),
+                                  );
+                                },
+                                text: 'Hesap Oluştur',
+                                options: FFButtonOptions(
+                                  width: 200,
+                                  height: 50,
+                                  color:
+                                      EsenDoTheme.of(context).primaryColor,
+                                  textStyle: EsenDoTheme.of(context)
+                                      .subtitle2
+                                      .override(
+                                        fontFamily: 'Lexend Deca',
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                  elevation: 3,
+                                  borderSide: BorderSide(
+                                    color: Colors.transparent,
+                                    width: 1,
+                                  ),
+                                  borderRadius: 8,
+                                ),
+                              ),
+                            ),
+                            FFButtonWidget(
+                              onPressed: () async {
+                                logFirebaseEvent('Button-ON_TAP');
+                                logFirebaseEvent('Button-Navigate-To');
+                                await Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => LoginWidget(),
+                                  ),
+                                );
+                              },
+                              text: 'Giriş Yap',
+                              options: FFButtonOptions(
+                                width: 200,
+                                height: 50,
+                                color: EsenDoTheme.of(context).white,
+                                textStyle: EsenDoTheme.of(context)
+                                    .subtitle2
+                                    .override(
+                                      fontFamily: 'Lexend Deca',
+                                      color: EsenDoTheme.of(context)
+                                          .primaryColor,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                elevation: 3,
+                                borderSide: BorderSide(
+                                  color: Colors.transparent,
+                                  width: 1,
+                                ),
+                                borderRadius: 8,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
