1fad95054bfe0eaf4e45854d2a49efd0d5cca500
Doğukan 'Esen' Balaman <dodikbalaman@gmail.com>
2022-05-27 14:22:30 +0300

    Minor Fixes

--- lib/index.dart
+++ lib/index.dart
@@ -1,1 +1,12 @@
+// Export pages
 export 'splash_screen/splash_screen_widget.dart' show SplashScreenWidget;
+export 'register/register_widget.dart' show RegisterWidget;
+export 'login/login_widget.dart' show LoginWidget;
+export 'my_tasks/my_tasks_widget.dart' show MyTasksWidget;
+export 'completed_tasks/completed_tasks_widget.dart' show CompletedTasksWidget;
+export 'create_task_page/create_task_page_widget.dart'
+    show CreateTaskPageWidget;
+export 'task_details/task_details_widget.dart' show TaskDetailsWidget;
+export 'my_profile/my_profile_widget.dart' show MyProfileWidget;
+export 'edit_profile/edit_profile_widget.dart' show EditProfileWidget;
+export 'change_password/change_password_widget.dart' show ChangePasswordWidget;
