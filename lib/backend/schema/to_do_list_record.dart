dbaa00c8b1297e2144bad20f24eadbe064da4a43
Doğukan 'Esen' Balaman <dodikbalaman@gmail.com>
2022-06-16 15:04:06 +0300

    Pages

--- lib/backend/schema/to_do_list_record.dart
+++ lib/backend/schema/to_do_list_record.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 
-import 'index.dart';
 import 'serializers.dart';
 import 'package:built_value/built_value.dart';
 
@@ -11,21 +10,16 @@ abstract class ToDoListRecord
   static Serializer<ToDoListRecord> get serializer =>
       _$toDoListRecordSerializer;
 
-  @nullable
-  DateTime get toDoDate;
+  DateTime? get toDoDate;
 
-  @nullable
-  String get toDoName;
+  String? get toDoName;
 
-  @nullable
-  String get toDoDescription;
+  String? get toDoDescription;
 
-  @nullable
-  bool get toDoState;
+  bool? get toDoState;
 
-  @nullable
   @BuiltValueField(wireName: kDocumentReferenceField)
-  DocumentReference get reference;
+  DocumentReference? get reference;
 
   static void _initializeBuilder(ToDoListRecordBuilder builder) => builder
     ..toDoName = ''
@@ -35,11 +29,11 @@ abstract class ToDoListRecord
   static CollectionReference get collection =>
       FirebaseFirestore.instance.collection('ToDoList');
 
-  static Stream<ToDoListRecord> getDocument(DocumentReference ref) => ref
+  static Stream<ToDoListRecord?> getDocument(DocumentReference ref) => ref
       .snapshots()
       .map((s) => serializers.deserializeWith(serializer, serializedData(s)));
 
-  static Future<ToDoListRecord> getDocumentOnce(DocumentReference ref) => ref
+  static Future<ToDoListRecord?> getDocumentOnce(DocumentReference ref) => ref
       .get()
       .then((s) => serializers.deserializeWith(serializer, serializedData(s)));
 
@@ -47,22 +41,22 @@ abstract class ToDoListRecord
   factory ToDoListRecord([void Function(ToDoListRecordBuilder) updates]) =
       _$ToDoListRecord;
 
-  static ToDoListRecord getDocumentFromData(
+  static ToDoListRecord? getDocumentFromData(
           Map<String, dynamic> data, DocumentReference reference) =>
       serializers.deserializeWith(serializer,
           {...mapFromFirestore(data), kDocumentReferenceField: reference});
 }
 
 Map<String, dynamic> createToDoListRecordData({
-  DateTime toDoDate,
-  String toDoName,
-  String toDoDescription,
-  bool toDoState,
+  DateTime? toDoDate,
+  String? toDoName,
+  String? toDoDescription,
+  bool? toDoState,
 }) =>
     serializers.toFirestore(
         ToDoListRecord.serializer,
         ToDoListRecord((t) => t
-          ..toDoDate = toDoDate
-          ..toDoName = toDoName
-          ..toDoDescription = toDoDescription
-          ..toDoState = toDoState));
+          ..toDoDate = toDoDate!
+          ..toDoName = toDoName!
+          ..toDoDescription = toDoDescription!
+          ..toDoState = toDoState!));
